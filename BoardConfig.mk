ec3fba16eee4fc38c9ef90b0a7efe995e35d1730
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-12-07 19:56:39 -0600

    jf: compress recovery ramdisk with lzma
    Change-Id: I1e590c22f0a68ecdbbb1c8057d6e55d9ec43b79f
    
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,8 +47,9 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.f
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_SOURCE := kernel/samsung/jf
+LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := aosp_jf_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/jf
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
